b69d18887d2c3d2ff5c541f86718fc72b9f36905
Muhammad Bayu Iskandar <bayu.iskandar.ums@gmail.com>
2017-03-02 06:19:47 +0800

    Add files via upload

---
+++ tugas_1.ino
@@ -0,0 +1,9 @@
+void setup()// fungsi yang akan berjalan sekali saja, digunakan untuk menginialisasi variable atau mode pin.
+{
+Serial.begin(9600);// untuk menentukan kecepatan transimisi data
+}
+ 
+void loop()// fungsi untuk mengeksekusi program secara berulang yang telah dibuat
+{
+Serial.println("Apa Kabar");// data yang dikirim berupa kalimat "Apa Kabar"
+}
